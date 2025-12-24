6e724c28f461ed34f29384c71263dfd4ecfc725e
your-local-leprechaun <lukemwulf@gmail.com>
2025-12-24 12:57:48 -0800

    Docstrings added

--- item/armor/IArmor.cs
+++ item/armor/IArmor.cs
@@ -3,7 +3,14 @@ namespace TombOfOcura.Item.Armor
 {
     public interface IArmor : IItem
     {
+        /*
+        Defense value provided by the armor
+        */
         int Defense { get; }
+
+        /*
+        Required level to equip the armor. Should use Vigor stat
+        */
         int RequiredLevel { get; }
     }
 }
